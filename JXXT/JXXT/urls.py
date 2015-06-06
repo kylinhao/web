e15dee7cf4d95e8866be9c1ac3c717298aa8e3c2
KylinHao <jianlinhaovic@163.com>
2015-06-06 22:54:42 +0800

    Student V1.2

--- JXXT/JXXT/urls.py
+++ JXXT/JXXT/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 
-
+# (r'^mydata/(?P<month>\w{3})/(?P<day>\d\d)/$', views.my_view),
 urlpatterns = [
     # Examples:
     # url(r'^$', 'JXXT.views.home', name='home'),
@@ -11,6 +11,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^login/', 'ustcjxxt.views.login'),
+    url(r'^student/$','ustcjxxt.views.student'),
+    url(r'^(.+)/(.+)/$','ustcjxxt.views.redirect_url'),
     url(r'^assistant/', 'ustcjxxt.views.assistant'),
     url(r"^logout/",'ustcjxxt.views.logout'),
 ]
--- JXXT/ustcjxxt/views.py
+++ JXXT/ustcjxxt/views.py
@@ -1,8 +1,8 @@
 # coding:utf-8
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from django.http import HttpResponseNotFound, HttpResponseRedirect,HttpResponse
-from .models import Student
+from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponse
+from .models import Student, Teacher, Assistant
 # from django.http import Http404
 # from django.template import RequestContext
 # from django.views.decorators.csrf import csrf_exempt
@@ -11,13 +11,35 @@ username = ""
 # @csrf_exempt
 def login(req):
     if req.method == 'POST':
-        userId = req.POST.get("userId", "")
+        user_id = req.POST.get("user_id", "")
         pwd = req.POST.get("password", "")
-        typeValue = req.POST.get("radio", "")
-        print typeValue
-        user = Student.objects.filter(stuId=userId, password=pwd)
+        type_value = req.POST.get("type", "")
+        if type_value == 'student':
+            user = Student.objects.filter(stuId=user_id, password=pwd)
+            print u'%s,%s' % (user_id, 'userId')
+            print u'%s,%s' % (pwd, 'pwd')
+            print u'%s,%s' % (type_value, 'type')
+            if user:
+                print u'%s,%s' % (user[0].name, 's')
+                req.session['user_id'] = user_id
+                return HttpResponseRedirect('/student/')
+        elif type_value == 'teacher':
+            user = Teacher.objects.filter(teaId=user_id, password=pwd)
+            print 't'
+            if user:
+                print u'%s,%s' % (user[0].name, 't')
+                req.session['user_id'] = user_id
+                return HttpResponseRedirect('/teacher/teaMain.html')
+        elif type_value == 'assistant':
+            user = Assistant.objects.filter(astId=user_id, password=pwd)
+            print 'a'
+            if user:
+                print u'%s,%s' % (user[0].name, 'a')
+                req.session['user_id'] = user_id
+                return HttpResponseRedirect('/assistant/')
+        else:
+            user = None
         if user:
-            req.session['username'] = user[0].name;
             return HttpResponseRedirect('/assistant/')
         else:
             return HttpResponseNotFound('<h1>用户名密码错误</h1>')
@@ -25,10 +47,39 @@ def login(req):
         return render_to_response('login.html', {})
 
 
-def assistant(req):
-    username = req.session.get('username','null')
+def student(req):
+    user_id = req.session.get('user_id', 'null')
+    if user_id != 'null':
+        user = Student.objects.filter(stuId=user_id)
+        return render_to_response('./student/stu-main.html', {"user": user[0]})
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+def redirect_url(req, type, url):
+    url = './' + type + '/' + url + ".html"
+    print url
+    user_id = req.session.get('user_id', 'null')
     if username != 'null':
-        return render_to_response('assistant.html',{"username":username})
+        if type == 'student':
+            user = Student.objects.filter(stuId=user_id)
+        elif type == 'teacher':
+            user = Teacher.objects.filter(teaId=user_id)
+        elif type == 'assistant':
+            user = Assistant.objects.filter(astId=user_id)
+        else:
+            user = 0;
+        return render_to_response(url, {"user": user[0]})
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+def assistant(req):
+    user_id = req.session.get('user_id', 'null')
+    if username != 'null':
+        user = Assistant.objects.filter(astId=user_id)
+        print 'get'
+        return render_to_response('assistant.html', {"user": user})
     else:
         return HttpResponseRedirect('/login/')
 
